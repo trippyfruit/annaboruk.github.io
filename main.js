2ecfb5366689aed3d673fe69e576ce2227a08003
Anna Boruk <66211501+trippyfruit@users.noreply.github.com>
2020-05-31 22:54:23 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ function showCandle() {
 	var icon = document.getElementById("elementIcon");
 	var products = document.getElementById("products");
 
+	var btnSmallCandle = document.getElementById("btnSmallCandle");
+	var btnSmallVisulka = document.getElementById("btnSmallVisulka");
+	var btnSmallCandle = document.getElementById("btnSmallCandle");
+
+	var thumbnailsBig = document.getElementById("thumbnailsBig");
+
 	candle.classList.remove("hidden");
 	visulka.classList.add("hidden");
 	icon.classList.add("hidden");
 
 	products.classList.remove("hidden");
+
+	btnSmallCandle.classList.add("hidden");
+	btnSmallVisulka.classList.remove("hidden");
+	btnSmallIcon.classList.remove("hidden");
+
+	thumbnailsBig.classList.add("hidden");
 
 	// (this).toggleClass('active');
 }
@@ -25,6 +37,10 @@ function showVisulka() {
 
 	products.classList.remove("hidden");
 
+	btnSmallCandle.classList.remove("hidden");
+	btnSmallVisulka.classList.add("hidden");
+	btnSmallIcon.classList.remove("hidden");
+
 	// (this).toggleClass('active');
 }
 
@@ -39,6 +55,10 @@ function showIcon() {
 	icon.classList.remove("hidden");
 
 	products.classList.remove("hidden");
+
+	btnSmallCandle.classList.remove("hidden");
+	btnSmallVisulka.classList.remove("hidden");
+	btnSmallIcon.classList.add("hidden");
 
 	// (this).toggleClass('active');
 }
